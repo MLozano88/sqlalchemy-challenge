55cdad45f03eef2bce9dd871e10a023e4e0277f7
Mario Lozano <Mario@m.lozano1733.com>
2021-02-06 21:28:45 -0500

    framework for climate app

--- app.py
+++ app.py
@@ -1,1 +1,66 @@
-#HW code goes here
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def home():
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation"
+        f"/api/v1.0/stations"
+        f"/api/v1.0/tobs"
+        f"/api/v1.0/<start>"
+        f"/api/v1.0/<start>/<end>"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    return (
+      
+    )
+
+@app.route("/api/v1.0/stations")
+def stations():
+    return (
+       
+    )
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    return (
+
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
